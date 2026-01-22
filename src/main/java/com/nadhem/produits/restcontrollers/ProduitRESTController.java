8983f99eb35cd0fcbb44f49d15f6550f5224d9c0
abdouchirac2024 <abdouchirac411@gmail.com>
2026-01-22 16:22:31 +0100

    Créer le Web service REST permettant de supprimer un produit

--- src/main/java/com/nadhem/produits/restcontrollers/ProduitRESTController.java
+++ src/main/java/com/nadhem/produits/restcontrollers/ProduitRESTController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,14 @@ public class ProduitRESTController {
 	@PutMapping()
 	public Produit updateProduit(@RequestBody Produit produit) {
 	return produitService.updateProduit(produit);
+	}
+	
+	
+	//@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+	@DeleteMapping("/{id}")
+	public void deleteProduit(@PathVariable("id") Long id)
+	{
+	produitService.deleteProduitById(id);
 	}
 
 }
