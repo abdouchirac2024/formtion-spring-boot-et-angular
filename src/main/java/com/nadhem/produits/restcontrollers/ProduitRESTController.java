c9026f8f1631e83d96285a1e701cbede223d6775
abdouchirac2024 <abdouchirac411@gmail.com>
2026-01-22 15:32:40 +0100

    Créer le Web service REST permettant de créer un produit

--- src/main/java/com/nadhem/produits/restcontrollers/ProduitRESTController.java
+++ src/main/java/com/nadhem/produits/restcontrollers/ProduitRESTController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,13 @@ public class ProduitRESTController {
 	public Produit getProduitById(@PathVariable("id") Long id) {
 		
 	return produitService.getProduit(id);
+	}
+	
+	
+	//@RequestMapping(method = RequestMethod.POST)
+	@PostMapping()
+	public Produit createProduit(@RequestBody Produit produit) {
+	return produitService.saveProduit(produit);
 	}
 
 }
